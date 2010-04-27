f6b6556ad87e22e94a578444fab479a86ecf2ad6
Sven Fuchs <svenfuchs@artweb-design.de>
2010-04-27 17:19:13 +0200

    use middleware for looking up assets from the gem, add assets from adva-cms, add basic layout from adva-cms

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,7 +1,12 @@
 class Admin::BaseController < ActionController::Base
   helper Admin::BaseHelper
+  layout 'admin'
   
   def self.responder
     Admin::Responder
   end
+  
+  def current_user
+  end
+  helper_method :current_user
 end
