26a761788ba00e2ceef25d6f2f9f02e578a69b2e
Ingo Weiss <ingo@ingoweiss.com>
2010-06-09 13:04:25 +0200

    Page now accepts nested attributes for article

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,9 +3,11 @@ class Page < Section
   
   has_one :article, :foreign_key => 'section_id', :dependent => :destroy
   
+  accepts_nested_attributes_for :article
+  
   protected
     
     def create_initial_article
-      create_article :site => site, :title => title
+      create_article :site => site, :title => title unless article.present?
     end
 end
