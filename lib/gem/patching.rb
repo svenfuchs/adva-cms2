1899b61f1f1820e584b77d57a0df609819921d4f
Sven Fuchs <svenfuchs@artweb-design.de>
2010-06-09 15:39:43 +0200

    don't apply patches if gem is not loaded

--- lib/gem/patching.rb
+++ lib/gem/patching.rb
@@ -1,10 +1,12 @@
 module Gem
-  def self.patching(gem_name, targeted_gem_version, &block)
-    active_gem_version = Gem.loaded_specs[gem_name].version.to_s
-    if active_gem_version != targeted_gem_version
-      raise "PATCH targeting version #{targeted_gem_version} of #{gem_name} but active version is #{active_gem_version}"
-    else
-      yield
+  def self.patching(gem_name, target_version, &block)
+    if spec = Gem.loaded_specs[gem_name]
+      active_version = spec.version.to_s
+      if active_version != target_version
+        raise "PATCH targeting version #{target_version} of #{gem_name} but active version is #{active_version}"
+      else
+        yield
+      end
     end
   end
 end
