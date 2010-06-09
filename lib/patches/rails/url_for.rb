a46e1c1eb108aec8e2770cbff06ee5ba12cc16bc
Sven Fuchs <svenfuchs@artweb-design.de>
2010-06-09 13:48:14 +0200

    move url_for patch to lib/patches/rails

--- lib/patches/rails/url_for.rb
+++ lib/patches/rails/url_for.rb
@@ -1,10 +1,11 @@
 require 'gem/patching'
 
+# remove trailing '.1' from url
 Gem.patching('rails', '3.0.0.beta4') do 
   ActionDispatch::Routing::RouteSet.class_eval do
     def url_for_with_singleton_resource_patch(options)
       url = url_for_without_singleton_resource_patch(options)
-      url.sub(/\.\d+\Z/, '') # remove trailing '.1' from url
+      url.sub(/\.\d+\Z/, '')
     end
     alias_method_chain :url_for, :singleton_resource_patch
   end
