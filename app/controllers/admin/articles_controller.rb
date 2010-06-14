a067f8f82f610e79cbefd2c91d0de8b23ce4d1ff
Sven Fuchs <svenfuchs@artweb-design.de>
2010-06-14 16:13:16 +0200

    fix tests

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -10,7 +10,7 @@ class Admin::ArticlesController < Admin::BaseController
 
   def update
     article.update_attributes!(params[:article])
-    respond_with :edit, *resources
+    respond_with *resources
   end
 
   protected
