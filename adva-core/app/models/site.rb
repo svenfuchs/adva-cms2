86330a0e2389c18e5616d509897219c57aec7c5d
Sven Fuchs <svenfuchs@artweb-design.de>
2010-06-30 10:33:21 +0200

    product catalog

--- adva-core/app/models/site.rb
+++ adva-core/app/models/site.rb
@@ -6,7 +6,7 @@ class Site < ActiveRecord::Base
   validates_presence_of :host, :name, :title, :sections
   validates_uniqueness_of :host
 
-  belongs_to :site
+  belongs_to :account
   has_many :sections, :dependent => :destroy, :inverse_of => :site
   has_many :pages
 
