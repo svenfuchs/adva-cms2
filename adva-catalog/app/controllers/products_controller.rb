ae42ebfb2935f0c9950b0691f35d17e7b1883faf
Sven Fuchs <svenfuchs@artweb-design.de>
2010-07-01 12:22:00 +0200

    fix link on products/show (by having a section accessor/helper method on products_controller)

--- adva-catalog/app/controllers/products_controller.rb
+++ adva-catalog/app/controllers/products_controller.rb
@@ -1,3 +1,6 @@
 class ProductsController < BaseController
   nested_belongs_to :section
+
+  attr_reader :section
+  helper_method :section # TODO extend inherited_resources to always define these
 end
