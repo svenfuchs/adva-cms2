02ebd8daa1ebae21998d4764f28c2fd4b9592295
Sven Fuchs <svenfuchs@artweb-design.de>
2010-07-02 10:32:09 +0200

    extract remaining redirects

--- adva-blog/config/redirects.rb
+++ adva-blog/config/redirects.rb
@@ -1,7 +1,8 @@
 require 'adva/registry'
 
 Adva::Registry.set :redirect, {
-  'admin/blogs#update'    => lambda { |responder| [:edit, *responder.resources] },
+  'admin/blogs#update' => lambda { |responder| [:edit, *responder.resources] },
+  'admin/posts#index'  => lambda { |responder| responder.resources[0..-2]    },
   'admin/posts#create' => lambda { |responder| [:edit, *responder.resources] },
   'admin/posts#update' => lambda { |responder| [:edit, *responder.resources] }
 }
--- adva-core/app/controllers/admin/pages_controller.rb
+++ adva-core/app/controllers/admin/pages_controller.rb
@@ -1,5 +1,2 @@
 class Admin::PagesController < Admin::SectionsController
-  def show
-    redirect_to resources.unshift(:edit).push(resource.article)
-  end
 end
