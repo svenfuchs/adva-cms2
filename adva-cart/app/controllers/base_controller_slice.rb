71e39ab84dc8a0834bdef8a4d09c2de646e91e7e
Matthias Viehweger <kronn@kronn.de>
2010-07-22 16:59:52 +0200

    made current_cart available to all controllers and views

--- adva-cart/app/controllers/base_controller_slice.rb
+++ adva-cart/app/controllers/base_controller_slice.rb
@@ -2,4 +2,5 @@ BaseController.class_eval do
   def current_cart
     @current_cart ||= session[:cart_id] ? Cart.find(session[:cart_id]) : Cart.create.tap { |cart| session[:cart_id] = cart.id }
   end
+  helper_method :current_cart
 end
