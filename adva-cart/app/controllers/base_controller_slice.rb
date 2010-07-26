0ff17fca818511156934a430059d50613681a0af
Ingo Weiss <ingoweiss@Ingo-Weisss-MacBook-Pro.local>
2010-07-26 17:33:33 +0200

    Cart is now regenerated if id of non-existant cart is in cookie

--- adva-cart/app/controllers/base_controller_slice.rb
+++ adva-cart/app/controllers/base_controller_slice.rb
@@ -1,11 +1,9 @@
 BaseController.class_eval do
   def current_cart
     @current_cart ||= begin
-      if session[:cart_id]
-        Cart.find(session[:cart_id])
-      else
-        Cart.create(params[:cart]).tap { |cart| session[:cart_id] = cart.id }
-      end
+      cart = Cart.find(session[:cart_id]) if session[:cart_id] rescue nil
+      cart ||= Cart.create(params[:cart]).tap { |cart| session[:cart_id] = cart.id }
+      cart
     end
   end
   helper_method :current_cart
