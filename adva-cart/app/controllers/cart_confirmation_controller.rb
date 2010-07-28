4e56b44fc932c9cadb7413b0940ca683664903aa
Sven Fuchs <svenfuchs@artweb-design.de>
2010-07-28 13:45:20 +0200

    refactor cart views

--- adva-cart/app/controllers/cart_confirmation_controller.rb
+++ adva-cart/app/controllers/cart_confirmation_controller.rb
@@ -2,7 +2,7 @@ class CartConfirmationController < BaseController
   def create
     # TODO
     CartMailer.order_confirmation_email(current_site, current_cart).deliver
-    CartMailer.order_notification_email(current_site, User.find_by_email('admin@admin.org')).deliver
+    CartMailer.order_notification_email(current_site, current_cart, User.find_by_email('admin@admin.org')).deliver
     current_cart.destroy
     session.delete(:cart_id)
     redirect_to :controller => :cart_confirmation, :action => :show
--- adva-cart/app/models/cart_mailer.rb
+++ adva-cart/app/models/cart_mailer.rb
@@ -1,17 +1,20 @@
 class CartMailer < ActionMailer::Base
+  attr_reader :site, :cart, :admin
+  helper_method :site, :cart, :admin
+
   def order_confirmation_email(site, cart)
-    recipients    cart.email
-    from          "#{site.name} <notifications@#{site.host}>"
-    subject       "Your order confirmation"
-    sent_on       Time.now
-    body          { }
+    @site, @cart = site, cart
+
+    mail :to      => cart.email,
+         :from    => "#{site.name} <notifications@#{site.host}>",
+         :subject => "Your order confirmation"
   end
 
-  def order_notification_email(site, admin)
-    recipients    admin.email
-    from          "#{site.name} <notifications@#{site.host}>"
-    subject       "New order"
-    sent_on       Time.now
-    body          { }
+  def order_notification_email(site, cart, admin)
+    @site, @cart, @admin = site, cart, admin
+
+    mail :to      => admin.email,
+         :from    => "#{site.name} <notifications@#{site.host}>",
+         :subject => "New order"
   end
 end
