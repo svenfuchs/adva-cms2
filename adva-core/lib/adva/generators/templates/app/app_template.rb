4623d654e242fbb317abb6a2ae1a223544a6630d
Raphaela Wrede <rwrede@adva-business.com>
2010-08-01 14:09:30 +0200

    added simple_table gem to gem dependencies

--- adva-core/lib/adva/generators/templates/app/app_template.rb
+++ adva-core/lib/adva/generators/templates/app/app_template.rb
@@ -1,5 +1,6 @@
 gem 'devise', '1.1.rc2'
 gem 'simple_form'
+gem 'simple_table', '>=0.0.3'
 
 gem 'adva-core',      :path => "#{config[:gem_root]}/adva-core"
 gem 'adva-blog',      :path => "#{config[:gem_root]}/adva-blog"
