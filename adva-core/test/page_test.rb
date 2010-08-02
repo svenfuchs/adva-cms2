a69783bd450291f94173742cc795f7b26d6a8ec9
Sven Fuchs <svenfuchs@artweb-design.de>
2010-08-02 08:08:06 +0200

    move unit tests to their engines

---
+++ adva-core/test/page_test.rb
@@ -0,0 +1,28 @@
+require File.expand_path('../test_helper', __FILE__)
+
+module AdvaCore
+  class PageTest < Test::Unit::TestCase
+    attr_reader :page_params
+  
+    def setup
+      @site = Site.create(
+        :host => 'localhost:3000', 
+        :title => 'Site Title', 
+        :name => 'Site Name',
+        :sections_attributes => [{ :title => 'Home' }]
+      )
+      @page_params = {
+        :title => 'Page title',
+        :site_id => @site.id,
+        :article_attributes => { :title => 'Article Title', :body => 'Body' }
+      }
+    end
+  
+    test "page accepts nested attributes for article" do
+      page = Page.create(page_params)
+      assert !page.article.new_record?
+      assert_equal 'Body', page.article.body
+    end
+
+  end
+end
