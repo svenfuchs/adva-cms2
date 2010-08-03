8ed5029155bf00dc534b2f2b29b2bd518d582aee
Sven Fuchs <svenfuchs@artweb-design.de>
2010-08-03 10:04:00 +0200

    fix generated app gemfile approach

--- adva-core/lib/adva/generators/templates/app/app_template.rb
+++ adva-core/lib/adva/generators/templates/app/app_template.rb
@@ -1,2 +1,7 @@
-create_file 'Gemfile', "load '#{config[:gem_root]}/Gemfile'"
+gem_root = config[:gem_root]
+gemfile = File.read("#{gem_root}/Gemfile")
+gemfile = gemfile.gsub('../', '').gsub('__FILE__', "'#{gem_root}'")
+
+create_file 'Gemfile', gemfile
 remove_file 'public/index.html'
+
