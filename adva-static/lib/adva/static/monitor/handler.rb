61c7b52131e87c6469806b0a1f23ccee88244b61
Sven Fuchs <svenfuchs@artweb-design.de>
2010-09-02 23:28:55 +0200

    improve monitor

--- adva-static/lib/adva/static/monitor/handler.rb
+++ adva-static/lib/adva/static/monitor/handler.rb
@@ -1,111 +1,57 @@
+require 'observer'
 require 'watchr'
 require 'watchr/event_handlers/portable'
 
 module Adva
   class Static
     module Monitor
-      class Handler < ::Watchr::EventHandler::Portable
-        def listen(root, pattern)
-          @root, @pattern = root, pattern
-          @monitored_paths = glob
-          loop { trigger; sleep(0.25) }
+      class Handler
+        include Observable
+
+        def initialize(observable, pattern)
+          add_observer(observable)
+          @pattern = pattern
+          @current = Dir[pattern]
+          @mtime   = Time.now
         end
 
-        def glob
-          paths = Dir[@root.join(@pattern)].map {|path| Pathname(path).expand_path }
-          paths << Pathname.new(__FILE__) if paths.empty?
-          paths
+        def listen
+          loop { trigger; sleep(0.5) }
         end
 
-
-        def detect_event
-          if path = (glob - @monitored_paths).first
-            @monitored_paths << path
-            return [path, :created]
+        def trigger
+          events.each do |path, event|
+            changed(true)
+            notify_observers(path, event)
           end
-          
-          @monitored_paths.each do |path|
-            unless path.exist?
-              @monitored_paths.delete(path)
-              return [path, :deleted]
-            end
+        end
+
+        protected
+
+          def events
+            @last    = @current.dup
+            @current = Dir[@pattern]
+            created + modified + deleted
           end
 
-          mtime_path = @monitored_paths.max { |a,b| a.mtime <=> b.mtime }
-          # ctime_path = @paths.max { |a,b| a.ctime <=> b.ctime }
-          # atime_path = @paths.max { |a,b| a.atime <=> b.atime }
+          def modified
+            (@current & @last).each do |path|
+              mtime = File.mtime(path)
+              if mtime > @mtime
+                @mtime = mtime
+                return [[path, :modified]]
+              end
+            end && []
+          end
 
-          if    mtime_path.mtime > @reference_mtime then @reference_mtime = mtime_path.mtime; [mtime_path, :modified]
-          # elsif ctime_path.ctime > @ctime then @ctime = ctime_path.ctime; [ctime_path, :changed ]
-          # elsif atime_path.atime > @atime then @atime = atime_path.atime; [atime_path, :accessed]
-          else; nil; end
-        rescue Errno::ENOENT
-          retry
-        end
+          def created
+            (@current - @last).map { |path| [path, :created] }
+          end
+
+          def deleted
+            (@last - @current).map { |path| [path, :deleted] }
+          end
       end
     end
   end
 end
-
-
-# require 'observer'
-# require 'ruby-debug'
-#
-# module Adva
-#   class Static
-#     module Monitor
-#       class Handler
-#         include Observable
-#
-#         def initialize(observable, root)
-#           add_observer(observable)
-#           @root = root
-#           @mtime, @atime, @ctime = Time.now, Time.now, Time.now
-#           @paths = glob
-#         end
-#
-#         def listen
-#           loop { trigger; sleep(0.5) }
-#         end
-#
-#         def trigger
-#           path, type = detect_event
-#           notify(path, type) unless path.nil?
-#         end
-#
-#         private
-#
-#           def glob
-#             Dir[@root.join('**/*.yml')].map { |path| Pathname.new(path) }
-#           end
-#
-#         def detect_event
-#           puts "detecting ..."
-#           # unless path = (glob - @paths).first
-#           #   @paths << path
-#           #   return [path, :created]
-#           # end
-#           #
-#           # @paths.each do |path|
-#           #   unless path.exist?
-#           #     @paths.delete(path)
-#           #     return [path, :deleted]
-#           #   end
-#           # end
-#
-#           mtime_path = @paths.max { |a,b| a.mtime <=> b.mtime }
-#           # ctime_path = @paths.max { |a,b| a.ctime <=> b.ctime }
-#           # atime_path = @paths.max { |a,b| a.atime <=> b.atime }
-# p mtime_path
-# p mtime_path.mtime > @mtime
-#           if    mtime_path.mtime > @mtime then @mtime = mtime_path.mtime; [mtime_path, :modified]
-#           # elsif ctime_path.ctime > @ctime then @ctime = ctime_path.ctime; [ctime_path, :changed ]
-#           # elsif atime_path.atime > @atime then @atime = atime_path.atime; [atime_path, :accessed]
-#           else; nil; end
-#         rescue Errno::ENOENT
-#           retry
-#         end
-#       end
-#     end
-#   end
-# end
