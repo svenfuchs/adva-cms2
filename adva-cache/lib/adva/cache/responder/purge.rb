4471196ea46536b8321dbf1e69c6d95bb99a4d4c
Sven Fuchs <svenfuchs@artweb-design.de>
2010-09-05 16:14:14 +0200

    fix adva/references stubs

--- adva-cache/lib/adva/cache/responder/purge.rb
+++ adva-cache/lib/adva/cache/responder/purge.rb
@@ -2,7 +2,8 @@ module Adva
   class Cache
     module Responder
       module Purge
-        delegate :purges?, :purge?, :purge, :to => :controller
+        delegate :purge,  :to => :controller
+        delegate :purge?, :to => :'controller.class'
         
         def to_html
           purge(controller.resources.reject { |r| !r.respond_to?(:new_record?) }) if purge?(params[:action])
