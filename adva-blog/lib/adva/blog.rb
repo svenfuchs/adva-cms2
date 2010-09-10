41918b017ac823972ab3dfa68fcbfbb4446411a9
Sven Fuchs <svenfuchs@artweb-design.de>
2010-09-10 12:56:59 +0200

    make assets have_many_polymorph assetables (and refactor unit tests)

--- adva-blog/lib/adva/blog.rb
+++ adva-blog/lib/adva/blog.rb
@@ -10,8 +10,12 @@ module Adva
   class Blog < ::Rails::Engine
     include Adva::Engine
 
-    initializer 'adva-blog.require_section_types', :after => 'prosecco.started' do
+    initializer 'adva-blog.require_section_types' do
       config.to_prepare { require_dependency 'blog' }
+    end
+
+    initializer 'adva-blog.register_assetable_types' do
+      Adva::Registry.set(:assetable_types, [:posts])
     end
   end
 end
