f5f1db5fa52e86101efcda72727d48c070dc6db1
Sven Fuchs <svenfuchs@artweb-design.de>
2010-09-11 23:54:32 +0200

    gotta require adva-core/features/support/env.rb before other adva-core features support files

--- adva-user/features/support/env.rb
+++ adva-user/features/support/env.rb
@@ -1,2 +1,4 @@
+require File.expand_path('../../../../adva-core/features/support/env', __FILE__)
+
 files = Dir[File.expand_path('../../../../adva-core/features/{support,step_definitions}/**/*.rb', __FILE__)]
 files.each { |file| require file }
