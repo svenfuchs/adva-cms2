e14fa40622d9b1d821146f8650ef2fbc2b2d096f
Sven Fuchs <svenfuchs@artweb-design.de>
2010-09-12 13:38:57 +0200

    upgrade inherited_resources_helpers to 0.0.6

--- adva-core/app/controllers/base_controller.rb
+++ adva-core/app/controllers/base_controller.rb
@@ -2,20 +2,18 @@ require 'inherited_resources'
 require 'inherited_resources/helpers'
 require 'simple_table'
 
-ActionView::Base.send :include, SimpleTable
+ActionView::Base.send(:include, SimpleTable) # TODO should be in simple_table
 
 class BaseController < InheritedResources::Base
-  class << self
-    def responder
-      Adva::Responder
-    end
-  end
-  
   tracks :resource, :resources, :site => %w(.title .name)
 
   layout 'default'
   helper_method :account, :site
   delegate :account, :to => :site
+
+  def self.responder
+    Adva::Responder
+  end
 
   def site
     Site.first # TODO
