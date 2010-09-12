cd407a8175a1766341c38ea938af932a3edd0aa4
Sven Fuchs <svenfuchs@artweb-design.de>
2010-09-13 01:05:22 +0200

    add active_record 3 skip_callbacks mechanism

---
+++ adva-core/test/testing/skip_callbacks_test.rb
@@ -0,0 +1,48 @@
+require File.expand_path('../../test_helper', __FILE__)
+
+require 'testing/skip_callbacks'
+
+module AdvaCoreTests
+  module Testing
+    ActiveRecord::Migration.verbose = false
+    ActiveRecord::Schema.define :version => 0 do
+      create_table :skip_callbacks, :force => true do |t|
+        t.string :name
+      end
+    end
+
+    class SkipCallback < ActiveRecord::Base
+      set_table_name 'skip_callbacks'
+      before_create { |record| record.name = 'created' }
+      before_update { |record| record.name = 'updated' }
+    end
+  
+    class SkipCallbacksTest < Test::Unit::TestCase
+      test "calls callbacks" do
+        record = SkipCallback.create
+        assert_equal 'created', record.name
+        
+        record.save
+        assert_equal 'updated', record.name
+      end
+
+      test "skip_callbacks skips callbacks" do
+        record = SkipCallback.skip_callbacks { SkipCallback.create! }
+        assert_nil record.name
+      end
+
+      test "skip_callbacks skips, then calls a callback" do
+        record = SkipCallback.skip_callbacks { SkipCallback.create! }
+        record.save
+        assert_equal 'updated', record.name
+      end
+      
+      test "without_callbacks skips callbacks" do
+        record = SkipCallback.without_callbacks.create!
+        assert_nil record.name
+      end
+    end
+  end
+end
+
+
