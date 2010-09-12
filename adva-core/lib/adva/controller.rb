0e0a4d244488fa6fcf614d313eef9bd7e75ac33e
Sven Fuchs <svenfuchs@artweb-design.de>
2010-09-12 13:30:39 +0200

    rename adva/internal_redirect and adva/references to adva/controller/internal_redirect and adva/controller/references

---
+++ adva-core/lib/adva/controller.rb
@@ -0,0 +1,7 @@
+module Adva
+  module Controller
+    autoload :AbstractActions,  'adva/controller/abstract_actions'
+    autoload :References,       'adva/controller/references'
+    autoload :InternalRedirect, 'adva/controller/internal_redirect'
+  end
+end
