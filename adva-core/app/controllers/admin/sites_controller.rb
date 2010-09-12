1653f333a537d5ccfd5a40931eded6fd2c3be624
Sven Fuchs <svenfuchs@artweb-design.de>
2010-09-12 16:12:42 +0200

    site creation needs to set the site's account

--- adva-core/app/controllers/admin/sites_controller.rb
+++ adva-core/app/controllers/admin/sites_controller.rb
@@ -1,5 +1,6 @@
 class Admin::SitesController < Admin::BaseController
   before_filter :set_params_for_nested_resources, :only => [:new, :edit]
+  before_filter :set_account, :only => :create
 
   helper :sections
 
@@ -7,5 +8,9 @@ class Admin::SitesController < Admin::BaseController
 
     def set_params_for_nested_resources
       params[:site] ||= { :host => request.host, :sections_attributes => [{ :title => '' }] }
+    end
+
+    def set_account
+      params[:site][:account_id] = account.id
     end
 end
