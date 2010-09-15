ed3d3a3e4d3d6c12611301bba6db4351315516bb
Sven Fuchs <svenfuchs@artweb-design.de>
2010-09-15 20:42:59 +0200

    add a subtitle

--- adva-core/db/migrate/20100415000001_adva_core_create_sites.rb
+++ adva-core/db/migrate/20100415000001_adva_core_create_sites.rb
@@ -5,7 +5,7 @@ class AdvaCoreCreateSites < ActiveRecord::Migration
       t.string :name,  :default => '', :null => false
       t.string :host,  :default => '', :null => false
       t.string :title, :default => '', :null => false
-      t.string :subtitle
+      t.string :subtitle, :default => ''
       t.string :timezone
       t.timestamps
     end
