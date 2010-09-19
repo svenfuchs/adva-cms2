9687c0b5d61560a3b65a8ff840b3a72785e2248c
Sven Fuchs <svenfuchs@artweb-design.de>
2010-09-19 16:21:41 +0200

    fix abstract_actions to work in both directions

--- adva-core/lib/adva/controller/abstract_actions.rb
+++ adva-core/lib/adva/controller/abstract_actions.rb
@@ -17,8 +17,7 @@ module Adva
             class_inheritable_accessor :abstract_action_options
             self.abstract_action_options = options.reverse_merge(:except => [])
 
-            before_filter :internal_redirect_to_concretion,
-              options.merge(:if => lambda { |c| c.instance_of?(Admin::SectionsController) })
+            before_filter :internal_redirect
           end
         end
 
@@ -28,39 +27,57 @@ module Adva
       end
 
       module InstanceMethods
-        # redirects requests to an abstract controller action (e.g. sections#show)
-        # to the concrete controller (e.g. pages#show)
-        def internal_redirect_to_concretion
-          params[instance_name] = params.delete(base_controller_name.singularize)
-          internal_redirect_to("#{concrete_controller_path}##{params[:action]}")
-        end
-
-        # redirects the view lookup to section type paths for all actions except
-        # :index. e.g. if the resource is a page then the show view will be looked
-        # up in views/admin/pages, not view/admin/sections
-        def _prefix
-          concrete_controller_path
-        end
-
-        def concrete_controller_path
-          "#{controller_namespace}/#{concrete_action? ? base_controller_name : collection_name}"
-        end
-        
-        def concrete_action?
-          Array(abstract_action_options[:except]).include?(params[:action])
-        end
-
-        def base_controller_name
-          @base_name ||= begin
-            base = self.class
-            base = base.superclass until base.superclass.controller_name == 'base'
-            base.controller_name
+        def internal_redirect
+          if redirect_to_concretion?
+            internal_redirect_to_concretion
+          elsif redirect_to_abstraction?
+            internal_redirect_to_abstraction
           end
         end
 
-        def controller_namespace
-          @controller_namespace ||= controller_path.split('/')[0..-2].join('/')
-        end
+        protected
+
+          def redirect_to_abstraction?
+            concrete_controller? && !concrete_action?
+          end
+
+          def redirect_to_concretion?
+            !concrete_controller? && concrete_action?
+          end
+
+          def internal_redirect_to_abstraction
+            params[abstract_instance_name] = params.delete(instance_name)
+            internal_redirect_to("#{abstract_controller_class.controller_path}##{params[:action]}")
+          end
+
+          def internal_redirect_to_concretion
+            params[instance_name] = params.delete(abstract_instance_name)
+            internal_redirect_to("#{controller_namespace}/#{collection_name}##{params[:action]}")
+          end
+
+          def concrete_controller?
+            !self.instance_of?(abstract_controller_class)
+          end
+
+          def concrete_action?
+            !Array(abstract_action_options[:except]).include?(params[:action].to_sym)
+          end
+
+          def abstract_controller_class
+            @abstract_controller_class ||= begin
+              abstract = self.class
+              abstract = abstract.superclass until abstract.superclass.controller_name == 'base'
+              abstract
+            end
+          end
+
+          def abstract_instance_name
+            @abstract_instance_name ||= abstract_controller_class.controller_name.singularize
+          end
+
+          def controller_namespace
+            @controller_namespace ||= controller_path.split('/')[0..-2].join('/')
+          end
       end
 
       ActionController::Base.extend(ActMacro)
