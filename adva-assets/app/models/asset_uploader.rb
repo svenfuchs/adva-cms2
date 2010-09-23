75b14d96276939cdaecd38a9bf0515b25536c959
Sven Fuchs <svenfuchs@artweb-design.de>
2010-09-23 15:21:04 +0200

    use Rails.env.test?

--- adva-assets/app/models/asset_uploader.rb
+++ adva-assets/app/models/asset_uploader.rb
@@ -12,7 +12,7 @@ class AssetUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored
   def store_dir
-    if Rails.env == 'test' and Rails.root.nil?
+    if Rails.env.test? and Rails.root.nil?
       "/tmp/sites/site-#{model.site_id}/assets/#{mounted_as}/#{model.id}"
     else
       "sites/site-#{model.site_id}/assets/#{mounted_as}/#{model.id}"
