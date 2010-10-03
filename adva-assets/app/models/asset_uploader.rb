f914b0c791f411f5dc218169939500c26a2773e3
Raphaela Wrede <rwrede@adva-business.com>
2010-10-03 12:16:18 +0200

    - now asset_uploader stores all files under /tmp if Rails.env is test - in production a store_dir under public is used

--- adva-assets/app/models/asset_uploader.rb
+++ adva-assets/app/models/asset_uploader.rb
@@ -10,10 +10,10 @@ class AssetUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored
   def store_dir
-    if Rails.env.test? and Rails.root.nil?
-      "/tmp/sites/site-#{model.site_id}/assets/#{mounted_as}/#{model.id}"
+    if Rails.env.test?
+      "/tmp/sites/site-#{model.site_id}/assets/file/#{model.id}"
     else
-      "sites/site-#{model.site_id}/assets/#{mounted_as}/#{model.id}"
+      "sites/site-#{model.site_id}/assets/file/#{model.id}"
     end
   end
 
--- adva-assets/lib/testing/step_definitions.rb
+++ adva-assets/lib/testing/step_definitions.rb
@@ -6,13 +6,7 @@ Then /^I should (not )?see the image "([^"]*)"$/ do |see, image_title|
   else
     assert images.one?, "image with title '#{image_title}' could not be found"
     image_src = images.first.attributes["src"].value
-    # The current carrierwave gem at master stores files with file names in the format 'filename_of_uploaded_file?1234325123' with
-    # the suffix '?' + some random integer value.
-    # As we patched webrat to make file uploads work (see adva-core/lib/patches/webrat/upload_file.rb) and because this patch always
-    # stores a file with name 'rails.png', we need to strip that suffix to be able to check if the file can be found:
-    Rails.root.join('public', image_src).to_s =~ /\/(.*)\?.*/
-    file_url = Rails.root.join('public', $1)
-    assert File.exists?(file_url), "file '#{file_url}' for image '#{image_title}' does not exist"
+    assert File.exists?(image_src), "file '#{image_src}' for image '#{image_title}' does not exist"
   end
 end
 
@@ -21,8 +15,7 @@ Then /^I should see the video "([^"]*)"$/ do |video_title|
   videos = body.xpath("//param[@name='name' and @value='#{video_title}']/parent::object/child::param[@name='movie']")
   assert videos.one?, "video with title '#{video_title}' could not be found"
   video_src = videos.first.attributes["value"].value
-  file_url = Rails.root.to_s + '/public' + video_src
-  assert File.exists?(file_url), "file '#{file_url}' for video '#{video_title}' does not exist"
+  assert File.exists?(video_src), "file '#{video_src}' for video '#{video_title}' does not exist"
 end
 
 When /^I press the delete button for image with title "([^"]*)"$/ do |image_title|
