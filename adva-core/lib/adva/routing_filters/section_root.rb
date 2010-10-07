a4302d4787a9779b30869bbf761823ea832a5845
Sven Fuchs <svenfuchs@artweb-design.de>
2010-10-07 19:06:13 +0200

    stop memoizing home? in section_root filter (amazing that this never broke before)

--- adva-core/lib/adva/routing_filters/section_root.rb
+++ adva-core/lib/adva/routing_filters/section_root.rb
@@ -36,14 +36,13 @@ module RoutingFilter
         path =~ exclude
       end
       memoize :excluded?
-      
+
       def recognition(host)
         if site = Site.by_host(host) and root = site.sections.root
           anchor = anchors_segments[root.class.name] || raise("undefined url anchor segment for: #{root.class.name}")
           [%r(^(/[\w]{2})?(?:\/?)(/#{anchor}|\.|\?|/?\Z)), "/#{root.type.tableize}/#{root.id}"]
         end
       end
-      # memoize :recognition
 
       def remove_root_section!(path)
         path.sub!(%r(#{$2}/#{$3}/?), '') if path =~ generate_pattern && home?($3)
@@ -52,12 +51,10 @@ module RoutingFilter
       def generate_pattern
         @generate_pattern ||= %r(^(?:/[\w]{2})?(/(#{Section.types.map(&:tableize).join('|')})/([\d]+)(?:/|\.|\?|$)))
       end
-      # memoize :generate_pattern
 
       def home?(id)
         Section.find(id.to_i).try(:home?)
       end
-      memoize :home?
 
       def host(env)
         host, port = env.values_at('SERVER_NAME', 'SERVER_PORT')
