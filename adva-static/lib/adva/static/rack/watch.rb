29751d93b13355b7a1e3921c485f20ac5913cc32
Sven Fuchs <svenfuchs@artweb-design.de>
2010-10-10 20:00:00 +0200

    improve adva-static readme

--- adva-static/lib/adva/static/rack/watch.rb
+++ adva-static/lib/adva/static/rack/watch.rb
@@ -23,8 +23,6 @@ module Adva
           Adva.out.puts "\n#{event_type}: #{path}"
           import  = Adva::Static::Import.new(:source => dir)
           request = import.request_for(path)
-          # params  = request.params
-          # params.merge!('_method' => 'delete') if event_type == :deleted
           self.request('POST', request.path, request.params)
         end
 
