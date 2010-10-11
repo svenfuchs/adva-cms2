c005d538fc8c8fe1a9d0770ad33b2b4884d7cd38
Sven Fuchs <svenfuchs@artweb-design.de>
2010-10-11 11:14:02 +0200

    AssetAssignments accept nested attributes for :asset, :image, :video

--- adva-assets/app/models/asset_assignment.rb
+++ adva-assets/app/models/asset_assignment.rb
@@ -4,8 +4,12 @@ class AssetAssignment < ActiveRecord::Base
   belongs_to :video, :foreign_key => 'asset_id'
   belongs_to :assetable, :polymorphic => true
 
+  accepts_nested_attributes_for :asset
+  accepts_nested_attributes_for :image
+  accepts_nested_attributes_for :video
+
   # acts_as_double_polymorphic_join(
-  #   :guests => [:dogs, :cats], 
+  #   :guests => [:dogs, :cats],
   #   :eatens => [:cats, :birds]
   # )
 end
--- adva-assets/test/asset_uploader_test.rb
+++ adva-assets/test/asset_uploader_test.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../test_helper', __FILE__)
 
 class AssetUploaderTest < Test::Unit::TestCase
-
   test "assets are stored in the expected directory if Rails.env is not test and when Rails.root is not nil" do
     Rails.env.stubs(:test?).returns(false)
     asset_mock = mock('Asset', :site_id => 1, :id => 2)
@@ -17,5 +16,4 @@ class AssetUploaderTest < Test::Unit::TestCase
     uploader.stubs(:model).returns(asset_mock)
     assert_equal "/tmp/sites/site-1/assets/file/2", uploader.store_dir
   end
-
 end
