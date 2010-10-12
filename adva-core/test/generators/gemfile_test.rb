2d37977a45da54da815553b89f0471688b657613
Matthias Viehweger <kronn@kronn.de>
2010-10-12 15:26:57 +0200

    accomodate expected path in Gemfile for different CI setups

--- adva-core/test/generators/gemfile_test.rb
+++ adva-core/test/generators/gemfile_test.rb
@@ -4,25 +4,26 @@ require 'adva/generators/gemfile'
 
 module AdvaCoreTests
   class GemfileTest < Test::Unit::TestCase
-    attr_reader :source
+    attr_reader :source, :path
 
     def setup
       @source = File.expand_path('../../../../Gemfile', __FILE__)
+      @path = '(adva-cms2|builds/\d+|adva-cms2/workspace)'
     end
 
     test 'with_engines' do
       gemfile = Adva::Generators::Gemfile.new('/tmp', :source => source, :engines => [:core])
-      assert_match %r('adva-core'.*(adva-cms2|builds/\d+)/adva-core'), gemfile.send(:with_engines)
-      assert_no_match %r('adva-blog'.*(adva-cms2|builds/\d+)/adva-blog'), gemfile.send(:with_engines)
+      assert_match %r('adva-core'.*#{path}/adva-core'), gemfile.send(:with_engines)
+      assert_no_match %r('adva-blog'.*#{path}/adva-blog'), gemfile.send(:with_engines)
 
       gemfile = Adva::Generators::Gemfile.new('/tmp', :source => source, :engines => [:core, :blog])
-      assert_match %r('adva-core'.*(adva-cms2|builds/\d+)/adva-core'), gemfile.send(:with_engines)
-      assert_match %r('adva-blog'.*(adva-cms2|builds/\d+)/adva-blog'), gemfile.send(:with_engines)
+      assert_match %r('adva-core'.*#{path}/adva-core'), gemfile.send(:with_engines)
+      assert_match %r('adva-blog'.*#{path}/adva-blog'), gemfile.send(:with_engines)
     end
 
     test 'engine_lines' do
       gemfile = Adva::Generators::Gemfile.new('/tmp', :source => source, :engines => [:core])
-      assert_match %r('adva-core'.*(adva-cms2|builds/\d+)/adva-core'), gemfile.send(:engine_lines).first
+      assert_match %r('adva-core'.*#{path}/adva-core'), gemfile.send(:engine_lines).first
     end
 
     test 'engines w/ [:blog] inserts :core' do
