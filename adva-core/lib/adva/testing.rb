2af184aee958014c190495acde7fcfbd26b2e8c7
Sven Fuchs <svenfuchs@artweb-design.de>
2010-10-13 16:17:58 +0200

    migrate first, then require stuff in engine's testing setup

--- adva-core/lib/adva/testing.rb
+++ adva-core/lib/adva/testing.rb
@@ -3,7 +3,7 @@ module Adva
     autoload :Engine, 'adva/testing/engine'
 
     Rails::Engine.extend(Engine)
-    
+
     class << self
       def setup(options = {})
         Adva.out = StringIO.new('')
@@ -13,25 +13,25 @@ module Adva
         Adva.engines.each do |e|
           e.setup_load_paths
         end
-        
+
         Adva.engines.each do |e|
+          e.migrate
           e.require_patches
           e.preload_sliced_models
-          e.migrate
         end
 
         load_assertions
         load_factories
       end
-      
+
       def load_assertions
         Adva.engines.each { |e| e.load_assertions }
       end
-      
+
       def load_factories
         Adva.engines.each { |e| e.load_factories }
       end
-      
+
       def load_cucumber_support
         Adva.engines.each { |e| e.load_cucumber_support }
       end
@@ -39,7 +39,7 @@ module Adva
       def load_helpers
         Adva.engines.each { |e| e.load_helpers }
       end
-    
+
       def setup_logging(options)
         if log = options[:log]
           FileUtils.touch(log) unless File.exists?(log)
@@ -47,7 +47,7 @@ module Adva
           ActiveRecord::LogSubscriber.attach_to(:active_record)
         end
       end
-    
+
       def setup_active_record
         ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => ':memory:')
         ActiveRecord::Migration.verbose = false
