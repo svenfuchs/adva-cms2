f49ad7e972bf674e8e598aed288f7ee3e0e951f2
Sven Fuchs <svenfuchs@artweb-design.de>
2010-10-13 11:02:53 +0200

    fix categories routing filter to simply work with the params hash on the application side

--- adva-categories/test/routing_filter/categories_test.rb
+++ adva-categories/test/routing_filter/categories_test.rb
@@ -5,57 +5,62 @@ require 'page'
 
 module AdvaCategoriesTests
   class RoutingFilterCategoriesTest < Test::Unit::TestCase
-    attr_reader :filter, :site, :home, :foo, :foo_bar, :bar
+    attr_reader :filter, :site, :home, :foo, :bar, :params
 
     def setup
-      @site    = Factory(:site)
-      @home    = site.sections.first.reload
+      @site = Factory(:site)
+      @home = site.sections.first.reload
 
-      @foo     = home.categories.create!(:name => 'foo').reload
-      @foo_bar = home.categories.create!(:name => 'bar', :parent => foo).reload
-      @bar     = home.categories.create!(:name => 'bar').reload
+      @foo  = home.categories.create!(:name => 'foo').reload
+      @bar  = home.categories.create!(:name => 'bar', :parent => foo).reload
 
       @filter = RoutingFilter::Categories.new
       site.sections.reset
+
+      @params = {}
     end
 
-    test "recognizes /sections/1/categories/foo" do
-      assert_equal "/sections/#{home.id}/categories/#{foo.id}", recognize("/sections/#{home.id}/categories/foo")
+    test "recognizes /sections/1/categories/foo and sets the category param" do
+      assert_equal "/sections/#{home.id}", recognize("/sections/#{home.id}/categories/foo")
+      assert_equal foo.id, params[:category_id]
     end
 
-    test "recognizes /sections/1/categories/foo/bar" do
-      assert_equal "/sections/#{home.id}/categories/#{foo_bar.id}", recognize("/sections/#{home.id}/categories/foo/bar")
+    test "recognizes /sections/1/categories/foo/bar and sets the category param" do
+      assert_equal "/sections/#{home.id}", recognize("/sections/#{home.id}/categories/foo/bar")
+      assert_equal bar.id, params[:category_id]
     end
 
-    test "recognizes /sections/1/categories/foo.rss" do
-      assert_equal "/sections/#{home.id}/categories/#{foo.id}.rss", recognize("/sections/#{home.id}/categories/foo.rss")
+    test "recognizes /sections/1/categories/foo.rss and sets the category param" do
+      assert_equal "/sections/#{home.id}.rss", recognize("/sections/#{home.id}/categories/foo.rss")
+      assert_equal foo.id, params[:category_id]
     end
 
-    test "recognizes /sections/1/categories/foo?foo=bar" do
-      assert_equal "/sections/#{home.id}/categories/#{foo.id}?foo=bar", recognize("/sections/#{home.id}/categories/foo?foo=bar")
+    test "recognizes /sections/1/categories/foo?foo=bar and sets the category param" do
+      assert_equal "/sections/#{home.id}?foo=bar", recognize("/sections/#{home.id}/categories/foo?foo=bar")
+      assert_equal foo.id, params[:category_id]
     end
 
-    test "generates from /sections/1/categories/1" do
-      assert_equal "/sections/#{home.id}/categories/foo/bar", generate("/sections/#{home.id}/categories/#{foo_bar.id}")
+    test "generates from /sections/1/categories/1 and a category param" do
+      assert_equal "/sections/#{home.id}/categories/foo/bar", generate("/sections/#{home.id}", :category_id => bar.id)
     end
 
-    test "generates from /sections/1/categories/1.rss" do
-      assert_equal "/sections/#{home.id}/categories/foo/bar.rss", generate("/sections/#{home.id}/categories/#{foo_bar.id}.rss")
+    test "generates from /sections/1/categories/1.rss and a category param" do
+      assert_equal "/sections/#{home.id}/categories/foo/bar.rss", generate("/sections/#{home.id}.rss", :category_id => bar.id)
     end
 
-    test "generates from /sections/1/categories/1?foo=bar" do
-      assert_equal "/sections/#{home.id}/categories/foo/bar?foo=bar", generate("/sections/#{home.id}/categories/#{foo_bar.id}?foo=bar")
+    test "generates from /sections/1/categories/1?foo=bar and a category param " do
+      assert_equal "/sections/#{home.id}/categories/foo/bar?foo=bar", generate("/sections/#{home.id}?foo=bar", :category_id => bar.id)
     end
 
     protected
 
       def recognize(path)
-        filter.around_recognize(path, 'SERVER_NAME' => 'www.example.com') { }
+        filter.around_recognize(path, 'SERVER_NAME' => 'www.example.com') { params}
         path
       end
 
-      def generate(path)
-        filter.around_generate({}) { path }
+      def generate(path, params = {})
+        filter.around_generate(params) { path }
         path
       end
   end
