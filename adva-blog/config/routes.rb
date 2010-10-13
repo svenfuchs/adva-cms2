e35d63c122cb25a4313251b0da2e4cd2241bf4a8
Sven Fuchs <svenfuchs@artweb-design.de>
2010-10-13 11:01:47 +0200

    tighten the blog archive routes

--- adva-blog/config/routes.rb
+++ adva-blog/config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
     end
   end
 
-  match 'blogs/:id(/:year(/:month(/:day)))',      :to => 'blogs#show',     :as => :blog
-  match 'blogs/:blog_id/:year/:month/:day/:slug', :to => 'posts#show'
-  match 'blogs/:blog_id/*permalink',              :to => "posts#internal", :as => :blog_post
+  constraints :year => /\d{4}/, :month => /\d{1,2}/, :day => /\d{1,2}/ do
+    match 'blogs/:id(/:year(/:month(/:day)))',      :to => 'blogs#show', :as => :blog
+    match 'blogs/:blog_id/:year/:month/:day/:slug', :to => 'posts#show', :as => :blog_post
+  end
+
+  match 'blogs/:blog_id/*permalink', :to => "posts#internal", :as => :blog_post
 end
