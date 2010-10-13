6c8e450251e7aaae2f7b96194ec12b2bb7504720
Sven Fuchs <svenfuchs@artweb-design.de>
2010-10-13 23:45:25 +0200

    allow including anonymous modules to modules

--- adva-core/test/core_ext/include_anonymous_test.rb
+++ adva-core/test/core_ext/include_anonymous_test.rb
@@ -5,14 +5,39 @@ require 'core_ext/ruby/module/include_anonymous'
 module AdvaCoreTests
   class IncludeAnonymousTest < Test::Unit::TestCase
     def teardown
-      self.class.send(:remove_const, :A)
+      [:A, :B].each do |name|
+        self.class.send(:remove_const, name) rescue nil
+      end
     end
-    
+
     test 'anonymous include on a class' do
       class A
         include { def foo; 'foo' end }
       end
       assert_equal 'foo', A.new.foo
+    end
+
+    test 'anonymous include on a module' do
+      module A
+        include { def foo; 'foo' end }
+      end
+      class B
+        include A
+      end
+      assert_equal 'foo', B.new.foo
+    end
+
+    test 'multiple anonymous modules included into a module' do
+      module A
+        include { def foo; 'foo' end }
+      end
+      A.module_eval do
+        include { def foo; "#{super}-foo" end }
+      end
+      class B
+        include A
+      end
+      assert_equal 'foo-foo', B.new.foo
     end
   end
 end
