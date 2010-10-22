5b8a26cd313a423e989961a794095fd24cda8afe
Sven Fuchs <svenfuchs@artweb-design.de>
2010-10-22 16:11:35 +0200

    add #sidebar to the within step contexts

--- adva-core/lib/testing/step_definitions/within_steps.rb
+++ adva-core/lib/testing/step_definitions/within_steps.rb
@@ -1,9 +1,12 @@
 # http://mislav.uniqpath.com/2010/09/cuking-it-right/
 
+# Then I should see "foo" in the top menu
+
 scopes = {
   'in the title' => 'h1, h2, h3',
   'in the top menu' => '#top',
-  'in the action menu' => '#actions'
+  'in the action menu' => '#actions',
+  'in the sidebar' => '#sidebar'
 }
 scopes.each do |within, selector|
   Then /^(.+) #{within}$/ do |step|
