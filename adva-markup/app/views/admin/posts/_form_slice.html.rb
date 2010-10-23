47e11f453cb721cd8c6cf131d0c897fa163f34d5
Sven Fuchs <svenfuchs@artweb-design.de>
2010-10-24 00:02:23 +0200

    need to call super to not overwrite existing sidebar content

--- adva-markup/app/views/admin/posts/_form_slice.html.rb
+++ adva-markup/app/views/admin/posts/_form_slice.html.rb
@@ -2,6 +2,7 @@ require_dependency 'admin/posts/_form.html.rb'
 
 Admin::Posts::Form.include do
   def sidebar
+    super
     tab :options do
       form.input :filter, :as => :select, :collection => Adva::Markup.options
     end
