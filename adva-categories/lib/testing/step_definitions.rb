e63a2857f43d46240f88df3181ba9449c7d0b149
Sven Fuchs <svenfuchs@artweb-design.de>
2010-10-24 00:03:35 +0200

    step definitions for "object should be categorized as foo"

--- adva-categories/lib/testing/step_definitions.rb
+++ adva-categories/lib/testing/step_definitions.rb
@@ -9,3 +9,13 @@ When /^I drag the category "([^"]*)" below the category "([^"]*)"$/ do |category
   follow_redirect!
 end
 
+Then /^the ([\w]+) (name|title)d "([^"]+)" should be categorized as "([^"]+)"$/ do |model, attribute, value, category|
+  record = model.classify.constantize.where(attribute => value).first
+  assert record.categories.map(&:name).include?(category), "expected #{record.inspect} to be categorized as #{category}"
+end
+
+Then /^the ([\w]+) (name|title)d "([^"]+)" should not be categorized as "([^"]+)"$/ do |model, attribute, value, category|
+  record = model.classify.constantize.where(attribute => value).first
+  assert !record.categories.map(&:name).include?(category), "expected #{record.inspect} not to be categorized as #{category}"
+end
+
