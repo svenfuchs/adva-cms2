405cd7f5f9ba9454c07ba60108a4a07f1a58c08c
Raphaela Wrede <rwrede@adva-business.com>
2010-10-28 14:05:29 +0200

    fixes reorder link for categories

--- adva-categories/app/views/admin/categories/_menu.html.rb
+++ adva-categories/app/views/admin/categories/_menu.html.rb
@@ -11,7 +11,7 @@ class Admin::Categories::Menu < Adva::View::Menu::Admin::Actions
         item(:'.destroy', resource_path, :method => :delete, :confirm => t(:'.confirm_destroy', :model_name => resource.class.model_name.human))
       else
         item(:'.new', new_path)
-        item(:'.reorder', show_parent_path, :'data-resource_type' => parent_resource.class.name.underscore, :'data-sortable_type' => 'categories')
+        item(:'.reorder', show_parent_path, :class => 'reorder', :'data-resource_type' => parent_resource.class.name.underscore, :'data-sortable_type' => 'categories')
       end
     end
   end
