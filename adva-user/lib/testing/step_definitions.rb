259ad62445a9714a58d80ba9d8fc7f6eec34d28b
Raphaela Wrede <rwrede@adva-business.com>
2010-11-12 16:15:27 +0100

    improves performance of cucumber features with optimized log in step

--- adva-user/lib/testing/step_definitions.rb
+++ adva-user/lib/testing/step_definitions.rb
@@ -1,4 +1,11 @@
 Given /^I am signed in with "([^"]*)" and "([^"]*)"$/ do |email, password|
+  post user_session_path, :user => { :email => email, :password => password }
+end
+
+# This step should only be used for testing the login itself (login.feature)
+# Please, use the step 'Given I am signed in with "admin@admin.org" and "admin!"' in all
+# other features for performance reasons.
+Given /^I sign in with "([^"]*)" and "([^"]*)"$/ do |email, password|
   get new_user_session_path
   fill_in 'Email', :with => email
   fill_in 'Password', :with => password
