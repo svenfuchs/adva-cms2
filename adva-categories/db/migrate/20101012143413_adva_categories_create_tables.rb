7f3c582eb2281fe97a4ba62405fb29039f8a9dce
Raphaela Wrede <rwrede@adva-business.com>
2010-11-15 10:39:16 +0100

    adds indices on categorizations

--- adva-categories/db/migrate/20101012143413_adva_categories_create_tables.rb
+++ adva-categories/db/migrate/20101012143413_adva_categories_create_tables.rb
@@ -14,6 +14,10 @@ class AdvaCategoriesCreateTables < ActiveRecord::Migration
       t.belongs_to :categorizable, :polymorphic => true
       t.references :category
     end
+    add_index :categorizations, :category_id
+    add_index :categorizations, :categorizable_id
+    add_index :categorizations, [:category_id, :categorizable_id, :categorizable_type],
+      :unique => true, :name => 'index_categorizations_on_cat_and_categorizable'
   end
 
   def self.down
