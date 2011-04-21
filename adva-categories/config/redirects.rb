f7fb2e061b2810fdb6c7e51983cd53c526619691
Ingo Weiss <ingoweiss@Ingo-Weisss-Adva-MacBook-Pro.local>
2011-04-21 14:33:12 +0200

    add destroy redirect to adva-categories

--- adva-categories/config/redirects.rb
+++ adva-categories/config/redirects.rb
@@ -1,4 +1,5 @@
 Adva::Registry.set :redirect, {
   'admin/categories#create' => lambda { |c| c.edit_url },
-  'admin/categories#update' => lambda { |c| c.edit_url }
+  'admin/categories#update' => lambda { |c| c.edit_url },
+  'admin/categories#destroy' => lambda { |c| c.index_url }
 }
