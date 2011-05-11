8eb9e7bf15ec2cb598b416cd464c5f3290395ebe
Niklas Hofer <niklas+dev@lanpartei.de>
2011-05-11 18:07:13 +0200

    reimplemented "I should not be on .." for capybara

--- adva-core/lib/testing/step_definitions/common_steps.rb
+++ adva-core/lib/testing/step_definitions/common_steps.rb
@@ -369,3 +369,14 @@ Then /^the "([^"]*)" radio button should be checked$/ do |label|
   Then %Q~the "#{label}" checkbox should be checked~
 end
 
+Then /^(?:|I )should not be on (.+)$/ do |page_name|
+  current_path = URI.parse(current_url).path
+  if current_path.respond_to? :should
+    current_path.should_not == path_to(page_name)
+  else
+    assert_not_equal path_to(page_name), current_path
+  end
+end
+
+
+
