cd6f2380bab2c03fc7560993d9f5c68259097a7d
Niklas Hofer <niklas+dev@lanpartei.de>
2011-05-20 16:27:13 +0200

    define Factory :post #section lazily

--- adva-blog/lib/testing/factories.rb
+++ adva-blog/lib/testing/factories.rb
@@ -3,7 +3,7 @@ Factory.define :blog, :parent => :section, :class => Blog do |f|
 end
 
 Factory.define :post do |f|
-  f.section Blog.first
+  f.section { Blog.first }
   f.title  'Title'
   f.body   'Body'
 end
